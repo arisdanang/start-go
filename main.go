25aef61633cea24e410996b8a68a0b50271e53a0
arisdanang <arisdanang2810@gmail.com>
2025-05-11 22:19:24 +0700

    feat: learning how to declare variable

--- main.go
+++ main.go
@@ -4,6 +4,32 @@ import "fmt"
 
 func main(){
 	var nama string = "danang"
+
+	var status string
+	status = "single"
+
+	// type inference
+	job := "software engineer"
+	job = "frontend engineer"
+
+	// declare multi var
+	var pertama, kedua, ketiga string
+	pertama, kedua, ketiga = "first", "second", "third"
+
+	// declare multi var with type inference
+	keempat, kelima, keenam := "fourth", "fifth", "sixth"
+
+	// declare multi var with type inference and different type
+	// one, isHoliday, pi, greeting := 1, true, 3.14, "hi"
+
+
+	// declare var with new keyword
+	name := new(string)
+
+	fmt.Println(*name)
+
 	fmt.Println("Hello world", "icik bos")
-	fmt.Printf("hello %s \n", nama)
+	fmt.Printf("hello %s %s %s \n", nama, status, job)
+	fmt.Printf("%s %s %s \n", pertama, kedua, ketiga)
+	fmt.Printf("%s %s %s \n", keempat, kelima, keenam)
 }
