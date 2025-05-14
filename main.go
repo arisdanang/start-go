2599f5f017bf2fe2290ceb2ef5e1630057f8ebc5
arisdanang <arisdanang2810@gmail.com>
2025-05-14 22:33:19 +0700

    feat: learning data types

--- main.go
+++ main.go
@@ -3,33 +3,66 @@ package main
 import "fmt"
 
 func main(){
-	var nama string = "danang"
+	// var nama string = "danang"
 
-	var status string
-	status = "single"
+	// var status string
+	// status = "single"
 
 	// type inference
-	job := "software engineer"
-	job = "frontend engineer"
+	// job := "software engineer"
+	// job = "frontend engineer"
 
 	// declare multi var
-	var pertama, kedua, ketiga string
-	pertama, kedua, ketiga = "first", "second", "third"
+	// var pertama, kedua, ketiga string
+	// pertama, kedua, ketiga = "first", "second", "third"
 
 	// declare multi var with type inference
-	keempat, kelima, keenam := "fourth", "fifth", "sixth"
+	// keempat, kelima, keenam := "fourth", "fifth", "sixth"
 
 	// declare multi var with type inference and different type
 	// one, isHoliday, pi, greeting := 1, true, 3.14, "hi"
 
 
 	// declare var with new keyword
-	name := new(string)
+	// name := new(string)
 
-	fmt.Println(*name)
+	// fmt.Println(*name)
 
-	fmt.Println("Hello world", "icik bos")
-	fmt.Printf("hello %s %s %s \n", nama, status, job)
-	fmt.Printf("%s %s %s \n", pertama, kedua, ketiga)
-	fmt.Printf("%s %s %s \n", keempat, kelima, keenam)
+
+	// fmt.Println("Hello world", "icik bos")
+	// fmt.Printf("hello %s %s %s \n", nama, status, job)
+	// fmt.Printf("%s %s %s \n", pertama, kedua, ketiga)
+	// fmt.Printf("%s %s %s \n", keempat, kelima, keenam)
+
+	// tipe data - numerik non desimal
+	var positifNum uint8 = 70
+	var negativeNum = -1234523
+
+	fmt.Printf("bilangan positif: %d\n", positifNum)
+	fmt.Printf("bilangan negatif: %d\n", negativeNum)
+
+
+	// tipe data - numerik desimal
+	var decimalNum = 3.14
+
+	fmt.Printf("bilangan desimal: %f\n", decimalNum)
+	fmt.Printf("bilangan desimal: %.3f\n", decimalNum)
+
+	// tipe data - boolean
+	var isOpen bool = true
+
+	fmt.Printf("is it open?? %t\n", isOpen)
+
+	// tipe data - string
+	var message string = "hello world"
+
+	fmt.Printf("pesan: %s \n", message)
+
+	// using backtick
+	var introduce = `Nama saya "Aris Danang"
+	salam kenal
+	mari kita belajar golang`
+
+	fmt.Println(introduce)
+
 }
