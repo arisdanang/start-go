ae382d80d0239e12fc173f64d2cf85d4e4c78f2d
arisdanang <arisdanang2810@gmail.com>
2025-05-18 00:33:49 +0700

    feat: learning switch case

--- main.go
+++ main.go
@@ -112,4 +112,46 @@ func main(){
 		fmt.Println("jalan!!!")
 	}
 
+
+	// temp variable inside conditional statements
+	var point = 1777.3
+
+	if percent := point / 100; percent >= 100 {
+		fmt.Printf("%.1f%s perfect! \n", percent, "%")
+	} else if percent >= 70 {
+		fmt.Printf("%.1f%s good! \n", percent, "%")
+	} else {
+		fmt.Printf("%.1f%s not bad!\n", percent, "%")
+	}
+
+	// switch case
+	var score = 9
+
+	switch score {
+	case 8:
+		fmt.Println("perfect")
+	case 7,6,5,4:
+		fmt.Println("awesome")
+	default: {
+		fmt.Println("not bad")
+		fmt.Println("you can be better")
+	}
+	}
+
+	// switch case dengan gaya if-else
+	var age uint8 = 17;
+
+	switch {
+	case age >= 17 :
+		fmt.Println("dewasa")
+		fallthrough // keyword yg unik dan membingungkan hehehehe
+	case age < 17:
+		fmt.Println("anak kecil")
+	case age < 5:
+		fmt.Println("balita")
+	default:
+		fmt.Println("batita")
+	}
+
+
 }
