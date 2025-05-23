5355f9c0b0a02a039c357ad4612949eef48234a4
arisdanang <arisdanang2810@gmail.com>
2025-05-23 23:15:17 +0700

    fix: add declare const

--- 2-variable.go
+++ 2-variable.go
@@ -26,4 +26,42 @@ func main(){
 
 	fmt.Println(one,isHoliday,pi, greeting)
 
+	// konstanta
+	const firstName string  = "Aris"
+
+	fmt.Print("halo ", firstName, "! \n")
+
+	// println => mencetak dengan spasi antar arg dan baris baru di akhir
+	fmt.Println("aris danang")
+	fmt.Println("aris","danang")
+
+	// print => mencetak tanpa baris baru
+	fmt.Print("aris danang\n")
+	fmt.Print("aris","danang\n")
+	fmt.Print("aris", " ", "danang\n")
+
+	// deklarasi multi konstanta
+	const (
+		square = "persegi"
+		isExist bool = true
+		numeric uint8 = 23
+		floatNum = 2.3
+	)
+
+	fmt.Println(square,isExist,numeric, floatNum)
+
+	const (
+		a = "testing"
+		b
+	)
+
+	fmt.Println(a,b)
+
+	const satu, dua = 1, 2
+	const three, four, five string = "tiga", "empat", "lima"
+
+	fmt.Println(satu,dua,three,four,five)
+
+
+
 }
