529f7878878855b60b2531a4c28d0f4582dca08f
arisdanang <arisdanang2810@gmail.com>
2025-05-31 00:19:54 +0700

    feat: learn copy func

--- 6.slice.go
+++ 6.slice.go
@@ -30,14 +30,43 @@ func main(){
 
 	fmt.Println("-------------")
 
-
+	var bAnimals = animals[0:2]
 	// append new element
-	var cAnimals = append(animals, "fish")
+	var cAnimals = append(bAnimals, "fish")
 	fmt.Println(animals)
 	fmt.Println(cAnimals)
+	fmt.Println(bAnimals)
 
 	fmt.Println("-------------")
 
+	dst := make([]string, 3)
+	src := []string{"bird","dog","fish","lion"}
+	n := copy(dst,src)
+
+	fmt.Println(dst)
+	fmt.Println(src)
+	fmt.Println(n)
+
+	fmt.Println("-------------")
+
+
+	// access slice using 3 indexes
+	var people = []string{"Budi","Agung","Irwan"}
+	var aPeople = people[0:2]
+	var bPeople = people[0:2:2]
+
+	fmt.Println(people)
+	fmt.Println(len(people))
+	fmt.Println(cap(people))
+
+	fmt.Println(aPeople)
+	fmt.Println(len(aPeople))
+	fmt.Println(cap(aPeople))
+
+
+	fmt.Println(bPeople)
+	fmt.Println(len(bPeople))
+	fmt.Println(cap(bPeople))
 
 
 
